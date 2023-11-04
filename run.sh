1085f5b7d34dcb739486d887301860278f7a7858
Paulo Matias <matias@ufscar.br>
2023-11-04 14:17:49 -0300

    map backup and repeater dirs to container

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec docker run --rm --device /dev/ttyUSB0:/dev/ttyUSB0 -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --net=host gm300-dosbox
+exec docker run --rm --device /dev/ttyUSB0:/dev/ttyUSB0 -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --net=host -v ./backup:/gm300/backup -v ./repeater:/gm300/repeater gm300-dosbox
