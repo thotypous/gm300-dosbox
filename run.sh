0593c143804dbc2d58c4af0f5262b27ce4063757
Paulo Matias <matias@ufscar.br>
2023-11-06 13:40:16 -0300

    fix ttyUSB0 permissions for rootless containers

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-exec docker run --rm --device /dev/ttyUSB0:/dev/ttyUSB0 -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --net=host -v ./backup:/gm300/backup -v ./repeater:/gm300/repeater thotypous/gm300-dosbox
+exec docker run --rm \
+    --device /dev/ttyUSB0 --annotation run.oci.keep_original_groups=1 \
+    -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --net host \
+    -v ./backup:/gm300/backup -v ./repeater:/gm300/repeater \
+    thotypous/gm300-dosbox
